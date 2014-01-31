b251e2762626113da20fc4bba7089749e5da0127
Berg, Ken (DS) <Ken.Berg@adp.com>
2014-01-31 07:57:32 -0600

    Adding Versioning

--- src/NServiceBus.SLR/Properties/AssemblyInfo.cs
+++ src/NServiceBus.SLR/Properties/AssemblyInfo.cs
@@ -1,10 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+using NServiceBus.SLR;
+
 [assembly: InternalsVisibleTo("Tests")]
 [assembly: AssemblyTitle("NServiceBus.SLR")]
 [assembly: AssemblyDescription("")]
@@ -30,8 +31,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(Version.Number)]
+[assembly: AssemblyFileVersion(Version.Number)]
+[assembly: AssemblyInformationalVersion(Version.Number)]
---
+++ src/NServiceBus.SLR/Version.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NServiceBus.SLR
+{
+    internal static class Version
+    {
+        public const string Major = "1";
+        public const string Minor = "1";
+        public const string Revision = "11";
+
+        /// <summary>
+        /// Gets the full version number
+        /// </summary>
+        public const string Number = Major + "." + Minor + "." + Revision;
+    }
+}
